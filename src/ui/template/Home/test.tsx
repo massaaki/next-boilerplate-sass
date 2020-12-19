77032a10f79f8d8dbdbf7c979a02099ccbd53305
Massaaki <mauricio.massaaki@gmail.com>
2020-12-19 15:57:52 -0300

    tests: add testing-library && add first test

---
+++ src/ui/template/Home/test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import Home from '.'
+
+describe('<Home />', () => {
+  it('should render the heading', () => {
+    render(<Home />)
+    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
+  })
+})
