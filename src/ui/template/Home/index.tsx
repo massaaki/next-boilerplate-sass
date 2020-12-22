74f0271af0369e3d8593e7db23369a29636cab39
Massaaki <mauricio.massaaki@gmail.com>
2020-12-21 21:16:25 -0300

    feat(i18n): Add next-translate and config locales to pt-Br and en-US

--- src/ui/template/Home/index.tsx
+++ src/ui/template/Home/index.tsx
@@ -1,7 +1,11 @@
+import useTranslation from 'next-translate/useTranslation'
+
 const Home = () => {
+  const { t } = useTranslation()
   return (
     <>
       <h1>Home</h1>
+      <p>{t('common:title')}</p>
     </>
   )
 }
