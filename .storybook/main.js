c462f0830eefb02ae554393681ee9ead1da5574a
Massaaki <mauricio.massaaki@gmail.com>
2020-12-25 17:47:06 -0300

    tests(config/storybook): configure to recosign sass && fix prefech==null in links(created a decorator)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,24 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/ui/components/**/stories.tsx'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, "..", "src"),
+      "node_modules",
+    ]
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config
+  }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,41 @@
+import '../src/themes/global.scss';
+import { renderWithConfigs } from '../src/utils/tests/helpers';
+import { addDecorator } from '@storybook/react'
+import { RouterContext } from 'next/dist/next-server/lib/router-context'
+import { linkTo } from '@storybook/addon-links'
+import { startCase } from 'lodash'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+addDecorator((storyFn) => (
+  <RouterContext.Provider
+    value={{
+      route: "/",
+      pathname: '/',
+      asPath: '/',
+      query: {},
+      basePath: '',
+      push: (_url, as) => {
+        if (as) {
+          linkTo('Routes', as !== '/' ? startCase(as.toString()) : 'Index')()
+        }
+        return Promise.resolve(true)
+      },
+      replace: (_url, as) => {
+        if (as) {
+          linkTo('Routes', as !== '/' ? startCase(as.toString()) : 'Index')()
+        }
+        return Promise.resolve(true)
+      },
+      reload: () => { },
+      prefetch: async () => { },
+      back: () => { },
+      beforePopState: () => { },
+      isFallback: false,
+    }}
+  >
+    {storyFn()}
+  </RouterContext.Provider>
+))
+
+export const decorators = [
+  (Story) => renderWithConfigs(<Story />, 'storybook')
+]
